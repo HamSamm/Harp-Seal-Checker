1a06f45bf29e66cb40ff592e62a864b1ea555092
HamSamm <samuel.cai@ssc-spc.gc.ca>
2026-07-15 11:36:08 -0400

    Breaking the web app so I can get a screenshot for the docs

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Hardcoded FSDH Prefix Fix
-class FSDHProxyPrefixFix:
-    def __init__(self, wsgi_app):
-        self.wsgi_app = wsgi_app
+# class FSDHProxyPrefixFix:
+#     def __init__(self, wsgi_app):
+#         self.wsgi_app = wsgi_app
 
-    def __call__(self, environ, start_response):
-        # Force Flask to prefix every url_for() path with /app/FEWSC
-        environ["SCRIPT_NAME"] = "/app/FEWSC"
-        return self.wsgi_app(environ, start_response)
+#     def __call__(self, environ, start_response):
+#         # Force Flask to prefix every url_for() path with /app/FEWSC
+#         environ["SCRIPT_NAME"] = "/app/FEWSC"
+#         return self.wsgi_app(environ, start_response)
 
-app.wsgi_app = FSDHProxyPrefixFix(app.wsgi_app)
+# app.wsgi_app = FSDHProxyPrefixFix(app.wsgi_app)
 
 # Azure SAS variables and File Names
 AZURE_SAS_URI = os.environ.get("SAS")
